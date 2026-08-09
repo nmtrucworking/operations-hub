d80736aa46aefab9a70a8971b8cd8b97849d75e0
NGUYEN-MINH-TRUC <nmtruc.work@gmail.com>
2026-08-10 03:05:53 +0700

    feat(api): register tenant registration workflow

--- apps/api/src/tenants/tenants.module.ts
+++ apps/api/src/tenants/tenants.module.ts
@@ -1,5 +1,10 @@
 import { Module } from "@nestjs/common";
+import { TenantRegistrationsController } from "./tenant-registrations.controller";
+import { TenantRegistrationsService } from "./tenant-registrations.service";
 import { TenantsController } from "./tenants.controller";
 
-@Module({ controllers: [TenantsController] })
+@Module({
+  controllers: [TenantsController, TenantRegistrationsController],
+  providers: [TenantRegistrationsService]
+})
 export class TenantsModule {}
